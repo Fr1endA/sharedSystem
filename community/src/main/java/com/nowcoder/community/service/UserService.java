132712211535d23808070c77623a9863b31c73b2
fr1enda <fr1enda@163.com>
2023-06-18 11:38:36 +0800

    2023.06.17 开发了登陆注册模块

--- community/src/main/java/com/nowcoder/community/service/UserService.java
+++ community/src/main/java/com/nowcoder/community/service/UserService.java
@@ -2,22 +2,123 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.dao.mapper.UserMapper;
+import com.nowcoder.community.until.CommunityConstant;
+import com.nowcoder.community.until.CommunityUtil;
+import com.nowcoder.community.until.EmailClient;
+import io.micrometer.common.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 //service调用dao层接口
 //面向接口编程
 //调用方法
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
 
 
     @Autowired
     private UserMapper userMapper;
+
+
+    @Autowired
+    private EmailClient emailClient;
+
+    @Autowired(required = false)
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
 
     public User findUserById(int id){
         return userMapper.selectById(id);
 
     }
 
+    public Map<String,Object> register(User user){
+
+        Map<String,Object> map=new HashMap<>();
+
+        //空值判断
+        if(user==null){
+            throw new IllegalArgumentException("参数不为空");
+        }
+        if(StringUtils.isBlank((user.getUsername()))){
+            map.put("usernameMessage","账号不能为空！");
+            return map;
+        }
+        if(StringUtils.isBlank((user.getPassword()))){
+            map.put("passwordMessage","密码不能为空！");
+            return map;
+        }
+
+        if(StringUtils.isBlank((user.getEmail()))){
+            map.put("emailMessage","邮箱不能为空！");
+            return map;
+        }
+
+        //验证账号,邮箱是否已存在（调用usermapper）
+        User u=userMapper.selectByName(user.getUsername());
+        if(u!=null){
+            map.put("usernameMessage","该账号已被注册！");
+            return map;
+        }
+
+        u=userMapper.selectByEmail(user.getEmail());
+        if(u!=null){
+            map.put("emailMessage","该邮箱已被注册");
+            return map;
+        }
+
+        //通过验证， 注册用户
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5(user.getPassword()+user.getSalt()));
+        user.setType(0);
+        user.setStatus(0);
+        user.setActivationCode(CommunityUtil.generateUUID());
+        user.setHeaderUrl(String.format("http://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        //激活邮件
+        Context context=new Context();
+        context.setVariable("email",user.getEmail());
+//        http://localhost:8080/community/activiation/#{userid}/code
+
+        String url=domain+contextPath+"/activation/" + user.getId() + "/" +user.getActivationCode();
+        context.setVariable("url", url);
+
+        String content=templateEngine.process("/mail/activation",context);
+        emailClient.sendMail(user.getEmail(),"激活账号",content);
+
+        return map;
+    }
+
+    public int activation(int userId,String code){
+
+
+        User user=userMapper.selectById(userId);
+        if(user.getStatus()==1){
+            return ACTIVATION_REPEAT;
+        }else if(user.getActivationCode().equals(code)){
+            userMapper.updateStatus(userId,1);
+            return ACTIVATION_SUCCESS;
+        }else{
+            return ACTIVATION_FAILURE;
+        }
+
+
+
+    }
 }
--- community/src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ community/src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -43,4 +43,58 @@ class CommunityApplicationTests implements ApplicationContextAware {
 		System.out.println(alphaService);
 	}
 
+	@Test
+	public void testLeeCode(){
+
+
+		class Solution {
+			public static int[][] generateMatrix(int n) {
+
+				int[][] array=new int[n][n];
+				int circle=n/2;
+				circle+=n%2==0?0:1;
+				int round=0;
+				int num=1;
+
+				// 全部循环
+				while(round<circle){
+
+					int i,j;
+					for(j= round;j<n-1-round;j++){
+						array[round][j]=num;
+						num++;
+					}
+
+					for(i=round;i<n-1-round;i++){
+						array[i][j]=num;
+						num++;
+					}
+
+					for(j=n-1-round;j>round;j--){
+						array[i][j]=num;
+						num++;
+					}
+					for(i=n-1-round;i>round;i--){
+						array[i][j]=num;
+						num++;
+					}
+					if(num>n*n){
+						break;
+					}
+					round++;
+				}
+				return array;
+			}
+		}
+		int[][] array=new int[3][3];
+
+		array=Solution.generateMatrix(3);
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(array[i][j] + ",");
+			}
+			System.out.println("\n");
+		}
+	}
+
 }
